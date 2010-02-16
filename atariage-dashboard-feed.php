e74f594e3d7102d767d9677cf65ef4cbab09c5fb
doc4 <doc4@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-16 18:53:14 +0000

    git-svn-id: https://plugins.svn.wordpress.org/atariage-dashboard-feed/trunk@206586 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- atariage-dashboard-feed.php
+++ atariage-dashboard-feed.php
@@ -3,7 +3,7 @@
 Plugin Name: AtariAge Dashboard Feed
 Plugin URI: http://www.doc4design.com/plugins/atariage-dashboard-feed
 Description: Add the AtariAge RSS Feed to your WordPress Dashboard
-Version: 1.9
+Version: 2.0
 Author: Doc4
 Author URI: http://www.doc4design.com
 */
@@ -28,43 +28,104 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 The license is also available at http://www.gnu.org/copyleft/gpl.html
 
 *********************************************************************************/
- 
 
-// Register Dashboard Widget
-add_action('wp_dashboard_setup', 'AtariAge_register_dashboard_widget');
-function AtariAge_register_dashboard_widget() {
-	wp_add_dashboard_widget('dashboard_AtariAge', __('AtariAge Dashboard Feed', 'AtariAge'), 'dashboard_AtariAge');
-    add_action('admin_head', 'AtariAge_head', 999);
-}
-
-function AtariAge_head() {
-	echo '<link href="'.get_bloginfo('wpurl').'/wp-content/plugins/atariage-dashboard-feed/css/aaStyle.css" rel="stylesheet" type="text/css" />'."\n";
-}
+include_once(ABSPATH . WPINC . '/rss.php');
  
 // Print Dashboard Widget
-function dashboard_AtariAge($sidebar_args) {
-	global $wpdb;
+function dashboard_AtariAge() {
+	$tech_rss_feed = 'http://www.atariage.com/news/rss.php';
+    $widget_options = dashboard_AtariAge_Options();
+    $rss = fetch_rss($tech_rss_feed);
 	
-	include_once(ABSPATH . WPINC . '/rss.php');
-	$tech_rss_feed = "http://www.AtariAge.com/news/rss.php";
 	
-	echo "<div id='identity'></div>";
-	echo "<ul>";
-	
-	$rss = fetch_rss($tech_rss_feed);
-	$rss->items = array_slice($rss->items, 0, 6);
-	$channel = $rss->channel;
-	echo "<div id=\"button\"><a class=\"button\" href=\"http://www.atariage.com\">View all</a></div>";
-	
-	foreach ($rss->items as $item ) {
-		$parsed_url = parse_url(wp_filter_kses($item['link']));
-		echo "<li><a href=" . wp_filter_kses($item['link']) . ">" . wptexturize(wp_specialchars($item['title'])) . "</a></li>";
-		echo "<div class=\"pubdate\">" . wptexturize(wp_specialchars($item['pubdate'])) . "</div>";
-		echo "<p>" . wptexturize(wp_specialchars($item['description'])) . "</p>";
-	}
-	echo "</ul>";
+	if ( !empty($rss->items) ) {
+	     echo "<div id='identity'></div>";
+	     echo '<ul>';
+         $rss->items = array_slice($rss->items, 0, $widget_options['items']);
+         
+		 foreach ($rss->items as $item ) {
+                  $trlink = '<li><a href="' . wp_filter_kses($item['link']) . '">' . wptexturize(wp_specialchars($item['title'])) . '</a>';
 
-}
+                 if($widget_options['showtime']) {				
+                    $trlink .=  "<div class='rss-date'>".$item['pubdate']."</div>";
+				 } else {
+				    echo '';
+                 }
+
+                 if($widget_options['showexcerpt']) {
+	                $trlink .=  "<p>" . wptexturize(wp_specialchars(substr(strip_tags($item['description']), 0,10000) )) . "...</p>";
+                 }
+                
+                 echo '<p>';
+                 echo $trlink;
+                 echo '</p>'; 
+                 }
 
 
+                 echo '<li></ul>';
+				
+                 } else {
+				 
+                 echo '<p>' . __( 'This dashboard widget queries <a href="http://www.atariage.com">AtariAge</a> for their latest 
+				                   RSS (Really Simple Syndication) feed. Oddly enought, it has found none... yet. It\'s okay - Albert is surely working on it.', 'dashboard_AtariAge' ) . "</p>";
+		         }
+		         echo '<p class="textright"><a class="button" href="http://www.atariage.com">'.__('View all').'</a></p>';
+                 }
+	
+
+
+/* add Dashboard Widget via function wp_add_dashboard_widget() */
+function dashboard_AtariAge_Init() {
+	wp_add_dashboard_widget( 'dashboard_AtariAge', __( 'AtariAge Dashboard Feed' ), 'dashboard_AtariAge', 'dashboard_AtariAge_Setup');
+	add_action('admin_head', 'AtariAge_head', 999);
+    }
+
+function AtariAge_head() {
+	echo '<link href="'.get_bloginfo('wpurl').'/wp-content/plugins/atariage-dashboard-feed/style.css" rel="stylesheet" type="text/css" />'."\n";
+    }
+
+function dashboard_AtariAge_Options() {
+	$defaults = array( 'items' => 5, 'showtime' => 1, 'showURL' => 1, 'showexcerpt' => 1);
+	if ( ( !$options = get_option( 'dashboard_AtariAge' ) ) || !is_array($options) )
+		    $options = array();
+	return array_merge( $defaults, $options );
+    }
+
+function dashboard_AtariAge_Setup() {
+	$options = dashboard_AtariAge_Options();
+
+	if ( 'post' == strtolower($_SERVER['REQUEST_METHOD']) && isset( $_POST['widget_id'] ) && 'dashboard_AtariAge' == $_POST['widget_id'] ) {
+		 foreach ( array( 'items', 'showtime', 'showurl', 'showexcerpt' ) as $key )
+				 $options[$key] = $_POST[$key];
+		         update_option( 'dashboard_AtariAge', $options );
+    }
+		
 ?>
+
+<!-- add Dashboard configure panel -->
+<div id='identity-configure'></div>
+
+<p>
+ <label for="items"><?php _e('How many items would you like to display?', 'dashboard_AtariAge' ); ?>
+  <select id="items" name="items">
+   <?php for ( $i = 5; $i <= 20; $i = $i + 1 )
+          echo "<option value='$i'" . ( $options['items'] == $i ? " selected='selected'" : '' ) . ">$i</option>"; ?>
+  </select>
+ </label>
+</p>
+   
+<p>
+ <label for="showtime">
+  <input id="showtime" name="showtime" type="checkbox" value="1"<?php if ( 1 == $options['showtime'] ) echo ' checked="checked"'; ?> />
+  <?php _e('Show post date?', 'dashboard_AtariAge' ); ?>
+ </label>
+</p>
+	
+<p>
+ <label for="showexcerpt">
+  <input id="showexcerpt" name="showexcerpt" type="checkbox" value="1"<?php if ( 1 == $options['showexcerpt'] ) echo ' checked="checked"'; ?> />
+  <?php _e('Show excerpt?', 'dashboard_AtariAge' ); ?>
+ </label>
+</p>
+	
+<?php } add_action('wp_dashboard_setup', 'dashboard_AtariAge_Init'); ?>

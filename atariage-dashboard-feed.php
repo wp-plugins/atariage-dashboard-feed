6a7f20457a277b1e3e5d748c530ef5ef73f020fc
doc4 <doc4@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-21 15:45:48 +0000

    git-svn-id: https://plugins.svn.wordpress.org/atariage-dashboard-feed/tags/2.5@441551 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ atariage-dashboard-feed.php
@@ -0,0 +1,130 @@
+<?php
+/*
+Plugin Name: AtariAge Dashboard Feed
+Plugin URI: http://www.doc4design.com/plugins/atariage-dashboard-feed
+Description: Add the AtariAge RSS Feed to your WordPress Dashboard
+Version: 2.5
+Author: Doc4
+Author URI: http://www.doc4design.com
+*/
+
+/******************************************************************************
+
+Copyright 2008  Doc4 : info@doc4design.com
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+The license is also available at http://www.gnu.org/copyleft/gpl.html
+
+*********************************************************************************/
+
+include_once(ABSPATH . WPINC . '/rss.php');
+ 
+// Print Dashboard Widget
+function dashboard_AtariAge() {
+	$tech_rss_feed = 'http://www.atariage.com/news/rss.php';
+    $widget_options = dashboard_AtariAge_Options();
+    $rss = fetch_rss($tech_rss_feed);
+	
+	if ( !empty($rss->items) ) {
+	
+	     echo '<ul>';
+         $rss->items = array_slice($rss->items, 0, $widget_options['items']);
+         
+		 foreach ($rss->items as $item ) {
+                  $trlink = '<li><a href="' . wp_filter_kses($item['link']) . '">' . wptexturize(wp_specialchars($item['title'])) . '</a>';
+
+                 if($widget_options['showtime']) {				
+                    $trlink .=  "<div class='rss-date'>".$item['pubdate']."</div>";
+				 } else {
+				    echo '';
+                 }
+
+                 if($widget_options['showexcerpt']) {
+	                $trlink .=  "<p>" . wptexturize(wp_specialchars(substr(strip_tags($item['description']), 0,350) )) . "...</p>";
+                 }
+                
+                 echo '<p>';
+                 echo $trlink;
+                 echo '</p>'; 
+                 }
+
+
+                 echo '<li></ul>';
+				
+                 } else {
+				 
+                 echo '<p>' . __( 'This dashboard widget queries <a href="http://www.atariage.com">AtariAge</a> for their latest 
+				                   RSS (Really Simple Syndication) feed. Oddly enough, it hasn\'t found any... yet. It\'s okay - Albert is surely working on it.', 'dashboard_AtariAge' ) . "</p>";
+		         }
+		         echo '<p class="textright"><a class="button" href="http://www.atariage.com">'.__('View all').'</a></p>';
+                 }
+	
+
+
+/* add Dashboard Widget via function wp_add_dashboard_widget() */
+function dashboard_AtariAge_Init() {
+	wp_add_dashboard_widget( 'dashboard_AtariAge', __( 'AtariAge Dashboard Feed' ), 'dashboard_AtariAge', 'dashboard_AtariAge_Setup');
+	add_action('admin_head', 'AtariAge_head', 999);
+    }
+
+function AtariAge_head() {
+	echo '<link href="'.get_bloginfo('wpurl').'/wp-content/plugins/atariage-dashboard-feed/style.css" rel="stylesheet" type="text/css" />'."\n";
+    }
+
+function dashboard_AtariAge_Options() {
+	$defaults = array( 'items' => 5, 'showtime' => 1, 'showURL' => 1, 'showexcerpt' => 1);
+	if ( ( !$options = get_option( 'dashboard_AtariAge' ) ) || !is_array($options) )
+		    $options = array();
+	return array_merge( $defaults, $options );
+    }
+
+function dashboard_AtariAge_Setup() {
+	$options = dashboard_AtariAge_Options();
+
+	if ( 'post' == strtolower($_SERVER['REQUEST_METHOD']) && isset( $_POST['widget_id'] ) && 'dashboard_AtariAge' == $_POST['widget_id'] ) {
+		 foreach ( array( 'items', 'showtime', 'showurl', 'showexcerpt' ) as $key )
+				 $options[$key] = $_POST[$key];
+		         update_option( 'dashboard_AtariAge', $options );
+    }
+		
+?>
+
+<!-- add Dashboard configure panel -->
+<div id='identity-configure'></div>
+
+<p>
+ <label for="items"><?php _e('How many items would you like to display?', 'dashboard_AtariAge' ); ?>
+  <select id="items" name="items">
+   <?php for ( $i = 5; $i <= 20; $i = $i + 1 )
+          echo "<option value='$i'" . ( $options['items'] == $i ? " selected='selected'" : '' ) . ">$i</option>"; ?>
+  </select>
+ </label>
+</p>
+   
+<p>
+ <label for="showtime">
+  <input id="showtime" name="showtime" type="checkbox" value="1"<?php if ( 1 == $options['showtime'] ) echo ' checked="checked"'; ?> />
+  <?php _e('Show post date?', 'dashboard_AtariAge' ); ?>
+ </label>
+</p>
+	
+<p>
+ <label for="showexcerpt">
+  <input id="showexcerpt" name="showexcerpt" type="checkbox" value="1"<?php if ( 1 == $options['showexcerpt'] ) echo ' checked="checked"'; ?> />
+  <?php _e('Show excerpt?', 'dashboard_AtariAge' ); ?>
+ </label>
+</p>
+	
+<?php } add_action('wp_dashboard_setup', 'dashboard_AtariAge_Init'); ?>
---
+++ readme.txt
@@ -0,0 +1,65 @@
+=== AtariAge Dashboard Feed ===
+Contributors: Doc4
+Donate link: http://www.doc4design.com/donate
+Tags: plugin, fun, entertainment, atari, atariage, video games, gaming, retro video games, homebrew video games, atari age, albert yarusso, feed, feeds, rss, simple syndication
+Requires at least: 2.7
+Tested up to: 3.2.1
+Stable tag: 2.5
+
+AtariAge Dashboard Feed
+
+== Description ==
+
+= Plugin URL =
+http://www.doc4design.com/plugins/atariage-dashboard-feed/
+
+Based on the excellent plugin "Techorati Incoming Links" by Alberto Varela, we have modified this plugin to display the AtariAge RSS (Really Simple Syndication) Feed directly within your WordPress Admin Dashboard. 
+
+Version 2.5 has only a few aesthetic changes attached to it. 
+
+We have a great respect for AtariAge, and everything Atari for that matter, but there isn't enough time during the busy work week to check in with the site as we would like. To reconcile this issue we took a look at the increasing amount of web design involving WordPress and thought it was only logical to add the AtariAge RSS feed to our development sites. We took Alberto Varela's plugin, modified it, and added a little extra flair such as: the article's publication date, full content, and a fancy AtariAge icon to finalize the look.
+
+WordPress 2.7 users please download version 1.0.
+
+
+== Screenshots ==
+
+View Screenshots:
+http://www.doc4design.com/plugins/atariage-dashboard-feed
+
+
+== Updates ==
+
+* Updated for WordPress 3.2.1
+* 2.0: Added a configure panel
+* 2.0: Truncated excerpts
+* Compatible with WordPress 2.9.1
+* Updated for WordPress 2.8.2
+* Corrected css styles link
+* Updated for WordPress 2.8.1
+* Small adjustments to get the feed working again across all WordPress versions
+* Adjusted the overlapping publication date text for users of Internet Explorer.
+* Updated for WordPress 2.7
+
+
+
+== Installation ==
+
+To install the plugin just follow these simple steps:
+
+1. Download the plugin and expand it.
+2. Copy the atariage-dashboard-feed folder into your plugins folder ( wp-content/plugins ).
+3. Log-in to the WordPress administration panel and visit the Plugins page.
+4. Locate the AtariAge Dashboard Feed plugin and click on the activate link.
+5. Visit the Wordpress Dashboard to view the RSS feeds
+
+
+== Frequently Asked Questions ==
+
+= What does the "View All" link do? =
+
+It is a direct link to atariage.com
+
+= I'm not using WordPress 2.7+ yet! =
+
+Please download version 1.0 of this plugin
